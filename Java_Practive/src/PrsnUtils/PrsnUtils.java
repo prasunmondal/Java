30c2fa303161d4fd60ec08c90f7508c285d9c246
Prasun <pmandal@akamai.com>
2019-11-10 10:57:16 +0530

    Update PrsnUtils.java

--- Java_Practive/src/PrsnUtils/PrsnUtils.java
+++ Java_Practive/src/PrsnUtils/PrsnUtils.java
@@ -9,6 +9,14 @@ public class PrsnUtils {
 			System.out.print(array[i] + ", ");
 		}System.out.println(array[i] + "]");
 	}
+	
+	public static void printArray(Object[] array) {
+		int i=0;
+		System.out.print("[");
+		for(i=0; i<array.length-1; i++) {
+			System.out.print(array[i] + ", ");
+		}System.out.println(array[i] + "]");
+	}
 
 	public void main(String[] args) {
 
